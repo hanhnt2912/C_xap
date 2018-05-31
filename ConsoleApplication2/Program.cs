dc6c4a5e7e25d8d14d850c566c6f01c106888f33
hanhnt2912 <hanhntd00589@fpt.edu.vn>
2018-05-31 09:46:19 +0700

    C#

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            GenerateMenu();
+        }
+        private static List<Student> list = new List<Student>();
+        private static void AddStudent()
+        {
+            while (true)
+            {
+                Student student = new Student();
+                Console.WriteLine("vui long nhap ten:");
+                student.Name = Console.ReadLine();
+                Console.WriteLine("vui long nhap ten:");
+                student.Rollnumber = Console.ReadLine();
+                Console.WriteLine("vui long nhap email:");
+                student.Email = Console.ReadLine();
+                Console.WriteLine("vui long nhap so dien thoai:");
+                student.Phone = Console.ReadLine();
+                list.Add(student);
+            }
+        }
+
+        private static void DisplayStudent()
+        {
+            foreach (var student in list)
+            {
+                Console.WriteLine(student.Name);
+            }  
+        }
+
+        private static void SearchByName()
+        {
+            Console.WriteLine("vui long nhap ten sinh vien can tim:");
+            var searchkey = Console.ReadLine();
+            for (int i = 0; i < list.Count; i++)
+            {
+                Student student = list[i];
+                if (student.Name  ==  searchkey)
+                {
+                    Console.WriteLine("Found:");
+                    Console.WriteLine(student.Rollnumber + "-" + student.Name);
+                }
+            }
+        }
+
+        private static void GenerateMenu()
+        {
+            while (true)
+            {
+               Console.WriteLine("--------------Menu----------------"); 
+                Console.WriteLine("1. Nhap cac sinh vien");
+                Console.WriteLine("2. In ra cac sinh vien");
+                Console.WriteLine("3. Tim sinh vien theo ten");
+                Console.WriteLine("4. thoat");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Vui long nhap lua chon:");
+                var choice = Int32.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                  case 1:
+                      AddStudent();
+                      break;
+                  case 2:
+                      DisplayStudent();
+                      break;
+                  case 3:
+                      SearchByName();
+                      break;
+                  case 4:
+                      Environment.Exit(1);
+                      break;
+                }
+
+            }   
+            
+        }
+    }
+}
---
+++ ConsoleApplication2/Student.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApplication2
+{
+    public class Student
+    {
+        private string name;
+        private string rollnumber;
+        private string email;
+        private string phone;
+
+        public Student()
+        {
+        }
+
+        public Student(string name, string rollnumber, string email, string phone)
+        {
+            this.name = name;
+            this.rollnumber = rollnumber;
+            this.email = email;
+            this.phone = phone;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Rollnumber
+        {
+            get { return rollnumber; }
+            set { rollnumber = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        public override string ToString()
+        {
+            return this.Name + this.Rollnumber + this.Email + this.Phone;
+        }
+    }
+}
